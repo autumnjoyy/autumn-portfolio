88055fbd86a89694a2401fc9cf1b212482a65632
Autumn Anderson <Autumn@Jeffs-Air-2.lan>
2025-05-20 16:13:46 -0600

    Fix: removed missing Header import

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
-import Header from './Header';
 import Hero from './Hero';
 import Resume from './components/Resume';
-import Footer from './Footer';
 
 function App() {
   return (
     <div>
-      <Header />
       <Hero />
       <Resume />
-      <Footer />
     </div>
   );
 }
-
-export default App;
